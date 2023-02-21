c5759c993845630e857a8eb8a58f3b6ec4bdff74
Timothy Attah <timothyattah733@gmail.com>
2023-02-21 18:16:46 +0100

    Setup tsconfig.json file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,54 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
-  }
-}
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'error',
+    'no-console': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code
+
+        // choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+
+        project: './tsconfig.json',
+      },
+    },
+  },
+};
