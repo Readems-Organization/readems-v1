341a1814a418c14cb3a622a1715fb95f1b99c7ec
Timothy Attah <timothyattah733@gmail.com>
2023-02-25 05:31:15 +0100

    Setup heroku cli

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv-safe';
 import express from 'express';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
-console.log(process.env.MY_NAME);
-
 const PORT = 5000 || process.env.PORT;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on: ${PORT}`));
