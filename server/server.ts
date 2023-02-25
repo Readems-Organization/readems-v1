cc1d941cf3a4f3c7cf0a8cf41ce2610c34511d35
Timothy Attah <timothyattah733@gmail.com>
2023-02-25 07:42:53 +0100

    Fixed static file rendering

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,28 @@
 import dotenv from 'dotenv-safe';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(express());
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+});
 
 const PORT = 5000 || process.env.PORT;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./frontend/build'));
+  app.use(express.static('../frontend/dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
 
