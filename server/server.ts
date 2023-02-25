a32ac44a007ae57f3e80e9b2ecdfb3e6e86ed0e4
Timothy Attah <timothyattah733@gmail.com>
2023-02-25 08:28:00 +0100

    Change frontend modules

--- server/server.ts
+++ server/server.ts
@@ -10,19 +10,19 @@ app.use(express());
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
 });
 
 const PORT = 5000 || process.env.PORT;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/dist'));
+  app.use(express.static('../frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
 
