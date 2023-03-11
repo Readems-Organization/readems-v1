52da23faf6ca07ff742bd5b2a99df24f5b53bbfc
Timothy Attah <timothyattah733@gmail.com>
2023-03-11 05:34:49 +0100

    Added a home get route

--- server/server.ts
+++ server/server.ts
@@ -10,13 +10,13 @@ app.use(express());
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 // app.use(express.static('../frontend/build'));
 
 app.get('/', (req, res) => {
   res.send('Hello Readems');
 });
 
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
 });
